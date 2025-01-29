5e9739425b2e4cbedbcc49726af546be0e11ac8f
itsmekanak <kanaksoni159@.gmail.com>
2025-01-29 13:01:27 +0530

    29-01-25

---
+++ JavaBasic/src/co/in/basic/FirstClass.java
@@ -0,0 +1,19 @@
+package co.in.basic;
+
+/**
+ * Ye class program ka starting point hai jo "Hello World" print karega.
+ * 
+ * @author KanakSoni
+ * @version 3.1
+ */
+public class FirstClass {
+    
+    /**
+     * Main method jo "Hello World ....." ko print karega.
+     *
+     * @param args command-line arguments, lekin yahan hum use nahi kar rahe hain
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World ......");
+    }
+}
---
+++ JavaBasic/src/co/in/basic/ScannerClass.java
@@ -0,0 +1,41 @@
+package co.in.basic;
+
+import java.util.Scanner;
+
+/**
+ * Ye class user se input lene ke liye Scanner ka use karti hai.
+ * Program do numbers ke sum ko calculate karega jo user input karega.
+ * 
+ * @author AapkaNaam
+ * @version 1.0
+ */
+public class ScannerClass {
+    
+    /**
+     * Ye main method hai jo user se do numbers leta hai aur unka sum calculate karke print karta hai.
+     *
+     * @param args command-line arguments, jo yahan use nahi kiye ja rahe hain
+     */
+    public static void main(String[] args) {
+        // User se input lene ka message
+        System.out.println("Taking Input From User With help of scanner ");
+        
+        // Scanner object banaya gaya user input lene ke liye
+        Scanner sc = new Scanner(System.in);
+        
+        // First number input lene ke liye
+        System.out.println("Enter number 1");
+        int a = sc.nextInt();
+        
+        // Second number input lene ke liye
+        System.out.println("Enter number 2");
+        int b = sc.nextInt();
+        
+        // Do numbers ka sum calculate kiya gaya
+        int sum = a + b;
+        
+        // Result print karne ke liye
+        System.out.println("The sum of these numbers is ");
+        System.out.println(sum);
+    }
+}
